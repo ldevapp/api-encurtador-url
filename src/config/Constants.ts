a301512464d76fb700aa9a46521c9fd6c80460ea
Leandro <leandro_csilva@ymail.com>
2021-10-28 23:39:42 -0300

    Adicionado classe, config e conexao com mongodb

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,3 +1,4 @@
 export const config = {
-    API_URL: "http://localhost:5000"
+    API_URL: "http://localhost:5000",
+    MONGO_CONNECTION: 'mongodb+srv://dbuser-encurtador:<password>@cluster0.bdtuk.mongodb.net/encurtador-url?retryWrites=true&w=majority',
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import URLController from './controller/URLController';
 import express, { Request, Response } from 'express';
+import { MongoConnection } from './database/MongoConnection';
 
 const host = 'http://localhost';
 const port = 5000;
@@ -7,6 +8,9 @@ const port = 5000;
 const api = express();
 
 api.use(express.json())
+
+const database= new MongoConnection();
+database.connect();
 
 const urlController = new URLController();
 api.post('/shorten', urlController.shorten);
